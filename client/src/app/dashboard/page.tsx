d36707cc70d5e747c158cc5f46b2c05dfaf845cc
Jamie Smart <jamie.smart.10@gmail.com>
2025-06-17 15:51:48 -0400

    create DashboardHeader page

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,23 +1,10 @@
+import DashboardHeader from '@/lib/components/DashboardHeader'
+
 export default function Dashboard() {
-  // stub data
-  const name = 'Jamie' // from user
-  const bgColor = 'orangered' // from user
-  const n = 4
-  //
   return (
-    <div>
-      <div className="flex justify-around py-4 items-end bg-[#ffffff2b]">
-        <div className="flex items-end space-x-4 sm:space-x-8">
-          <div
-            className="flex items-center justify-center rounded-full w-12 h-12 border-4 border-white text-2xl"
-            style={{ backgroundColor: bgColor }}>
-            J
-          </div>
-          <span>Hi, {name}!</span>
-        </div>
-        <span>You have {n} upcoming trips!</span>
-      </div>
+    <>
+      <DashboardHeader />
       <div>Dashboard</div>
-    </div>
+    </>
   )
 }
