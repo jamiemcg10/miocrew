292d486851320a92533944330642bcddd2241077
Jamie Smart <jamie.smart.10@gmail.com>
2025-06-18 16:13:27 -0400

    update tests

--- client/src/lib/components/layout/Menu.cy.tsx
+++ client/src/lib/components/layout/Menu.cy.tsx
@@ -2,7 +2,56 @@ import React from 'react'
 import Menu from './Menu'
 
 describe('<Menu />', () => {
-  it('mounts', () => {
+  it('Mounts (>= sm)', () => {
     cy.mount(<Menu open={true} matches={true} handleClose={() => {}} />)
+  })
+
+  it('Mounts (< sm)', () => {
+    cy.mount(<Menu open={true} matches={false} handleClose={() => {}} />)
+  })
+
+  it('Has "Dashboard" option', () => {
+    cy.mount(<Menu open={true} matches={true} handleClose={() => {}} />)
+    cy.contains('Dashboard')
+  })
+
+  it('"Dashboard" option has correct href', () => {
+    cy.mount(<Menu open={true} matches={true} handleClose={() => {}} />)
+    cy.contains('Dashboard').closest('a').should('have.attr', 'href', '/dashboard')
+  })
+
+  it('Has "Schedule" option', () => {
+    cy.mount(<Menu open={true} matches={true} handleClose={() => {}} />)
+    cy.contains('Schedule')
+  })
+
+  it('Has "Tasks" option', () => {
+    cy.mount(<Menu open={true} matches={true} handleClose={() => {}} />)
+    cy.contains('Tasks')
+  })
+
+  it('Has "Planning" option', () => {
+    cy.mount(<Menu open={true} matches={true} handleClose={() => {}} />)
+    cy.contains('Planning')
+  })
+
+  it('Has "Expenses" option', () => {
+    cy.mount(<Menu open={true} matches={true} handleClose={() => {}} />)
+    cy.contains('Expenses')
+  })
+
+  it('Has "Messages" option', () => {
+    cy.mount(<Menu open={true} matches={true} handleClose={() => {}} />)
+    cy.contains('Messages')
+  })
+
+  it('Has "Past trips" option', () => {
+    cy.mount(<Menu open={true} matches={true} handleClose={() => {}} />)
+    cy.contains('Past trips')
+  })
+
+  it('Has "Settings" option', () => {
+    cy.mount(<Menu open={true} matches={true} handleClose={() => {}} />)
+    cy.contains('Settings')
   })
 })
