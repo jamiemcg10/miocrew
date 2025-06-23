be194b8b399734fb05655ad7b73e48625b4758c5
Jamie Smart <jamie.smart.10@gmail.com>
2025-06-23 17:28:45 -0400

    start laying out schedule

--- client/src/lib/components/event/Schedule.tsx
+++ client/src/lib/components/event/Schedule.tsx
@@ -1,11 +1,21 @@
 import { trips } from '@/lib/utils/dummyData'
 import { Button } from '@mui/material'
+import dayjs from 'dayjs'
 
 export default function Schedule() {
-  const tripStart = trips[0].startDate
-  const tripEnd = trips[0]?.endDate
+  const tripStart = dayjs(trips[0].startDate)
+  const tripEnd = dayjs(trips[0]?.endDate || tripStart)
 
   // calculate number of days
+  const tripLength = tripEnd.diff(tripStart, 'day') // (tripEnd - tripStart) / MS_PER_DAY
+
+  // for # days, add startDay + n to array {date:Date, events:?}[]
+  const days = []
+  for (let i = 0; i <= tripLength; i++) {
+    days.push(tripStart.add(i, 'day'))
+  }
+
+  console.log({ days })
 
   return (
     <>
@@ -15,17 +25,19 @@ export default function Schedule() {
             Add activity
           </Button>
         </div>
-        <div className="flex border border-[yellow] grow justify-between px-4">
-          <div className="border border-white w-[32%] h-full">
-            <div className="bg-[teal] rounded-sm">Event</div>
-          </div>
-
-          <div className="border border-white w-[32%]">
-            <div className="bg-[orangered] rounded-sm">Event</div>
-          </div>
-          <div className="border border-white w-[32%]">
-            <div className="bg-[violet] rounded-sm">Event</div>
-          </div>
+        <div
+          className="flex grow justify-between px-6 space-x-6 overflow-x-scroll"
+          style={{ scrollSnapType: 'x mandatory' }}>
+          {days.map((_day, i) => {
+            return (
+              <div
+                className="bg-[#29293A] w-full sm:w-[40%] md:w-[30%] shrink-0 h-full xbg-[#1a1a1a] rounded-sm"
+                key={i}
+                style={{ scrollSnapAlign: 'start', scrollMargin: i ? '64px' : '32px' }}>
+                <div className="bg-[teal] rounded-sm">Event</div>
+              </div>
+            )
+          })}
         </div>
       </div>
     </>
