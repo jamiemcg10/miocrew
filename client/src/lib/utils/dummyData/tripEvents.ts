740b231c9d6f1b61d6bb3554618cbf82e72f8a94
Jamie Smart <jamie.smart.10@gmail.com>
2025-06-23 12:28:45 -0400

    add tripEvents dummy data

---
+++ client/src/lib/utils/dummyData/tripEvents.ts
@@ -0,0 +1,44 @@
+export const tripEvents = [
+  {
+    id: 'wm1',
+    name: 'Check into AirBnB and relax',
+    description: 'Arrive, hang out at AirBnB until dinner',
+    location: '132 Sonoma Crest Road Ext',
+    startTime: new Date('August 5, 2026 2:00 PM'),
+    endTime: new Date('August 5, 2026 6:00 PM'),
+    color: 'orangered'
+  },
+  {
+    id: 'wm2',
+    name: 'Dinner at Gran Turismo',
+    location: '132 Sonoma Crest Road',
+    startTime: new Date('August 5, 2026 6:30 PM'),
+    endTime: new Date('August 5, 2026 8:30 PM'),
+    color: 'teal'
+  },
+  {
+    id: 'wm3',
+    name: 'Napa Wine Tour',
+    description: 'Bus tour with stops at 5 different vineyards',
+    location: '11824 Main St',
+    startTime: new Date('August 6, 2026 2:00 PM'),
+    endTime: new Date('August 6, 2026 6:00 PM'),
+    color: 'violet'
+  },
+  {
+    id: 'wm4',
+    name: 'Lunch at Josh Vineyards',
+    location: '414 Boston Ave.',
+    startTime: new Date('August 7, 2026 12:00 PM'),
+    endTime: new Date('August 7, 2026 1:30 PM'),
+    color: 'aliceblue'
+  },
+  {
+    id: 'wm5',
+    name: 'Club Xtrove',
+    location: '4155 Elm St.',
+    startTime: new Date('August 7, 2026 10:00 PM'),
+    endTime: new Date('August 8, 2026 3:00 AM'),
+    color: 'navy'
+  }
+]
