b662d14908cddeed7d0812355b6d2af17238e7d7
Jamie Smart <jamie.smart.10@gmail.com>
2025-06-26 15:01:06 -0400

    add autocomplete to crew box

--- client/src/app/(app)/create/TripForm.tsx
+++ client/src/app/(app)/create/TripForm.tsx
@@ -7,10 +7,20 @@ import TextField from '@mui/material/TextField'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import { DatePicker } from '@mui/x-date-pickers/DatePicker'
+import Autocomplete from '@mui/material/Autocomplete'
 
 const fieldStyles = { width: '100%' }
 
 export default function TripForm() {
+  const dummyOptions = [
+    { id: '1', email: 'johnny.appleseed@gmail.com' },
+    { id: '2', email: 'johnny.walker@gmail.com' },
+    { id: '3', email: 'daniel.radcliffe@gmail.com' },
+    { id: '4', email: 'emma.watson@gmail.com' },
+    { id: '5', email: 'robin.hood@gmail.com' },
+    { id: '6', email: 'katniss.everdeen@gmail.com' },
+    { id: '7', email: 'cole.sprouse@gmail.com' }
+  ]
   return (
     <>
       <div>
@@ -32,13 +42,35 @@ export default function TripForm() {
         <TextField variant="outlined" label="Description" multiline rows={4} sx={fieldStyles} />
       </div>
       <div>
-        <TextField
-          variant="outlined"
-          label="Crew"
-          multiline
-          minRows={2}
-          maxRows={3}
-          sx={fieldStyles}
+        <Autocomplete
+          id="add-crew"
+          options={dummyOptions}
+          getOptionLabel={(option) => (typeof option === 'string' ? option : option.email)}
+          multiple
+          freeSolo
+          filterOptions={(options, params) => {
+            const filtered = options.filter((option) =>
+              option.email.toLowerCase().includes(params.inputValue.toLowerCase())
+            )
+            if (
+              params.inputValue !== '' &&
+              !options.some((option) => option.email === params.inputValue)
+            ) {
+              filtered.push({ id: '', email: params.inputValue })
+            }
+            return filtered
+          }}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              variant="outlined"
+              label="Crew"
+              multiline
+              minRows={2}
+              maxRows={3}
+              sx={fieldStyles}
+            />
+          )}
         />
       </div>
       <Button
