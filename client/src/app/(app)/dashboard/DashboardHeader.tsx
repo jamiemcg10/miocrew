5dc5525ca65c80a9d9288a3a18993e938419423e
Jamie Smart <jamie.smart.10@gmail.com>
2025-06-30 15:52:36 -0400

    use Avatar in DashboardHeader

--- client/src/app/(app)/dashboard/DashboardHeader.tsx
+++ client/src/app/(app)/dashboard/DashboardHeader.tsx
@@ -1,3 +1,5 @@
+import Avatar from '@mui/material/Avatar'
+
 export default function DashboardHeader() {
   // stub data
   const user = {
@@ -12,12 +14,18 @@ export default function DashboardHeader() {
   return (
     <div className="flex justify-around py-4 items-center bg-[#cee2f5] dark:bg-white/20">
       <div className="flex items-center space-x-2 sm:space-x-4">
-        <div
-          className="flex items-center justify-center rounded-full w-12 h-12 border-4 border-white text-2xl"
-          style={{ backgroundColor: user.color }}>
+        <Avatar
+          alt={user.firstName}
+          sx={{
+            color: 'var(--foreground)',
+            backgroundColor: user.color,
+            border: '4px solid var(--foreground)',
+            height: 48,
+            width: 48
+          }}>
           {user.firstName.charAt(0)}
           {user.lastName.charAt(0)}
-        </div>
+        </Avatar>
         <span className="font-bold">Hi, {user.firstName}!</span>
       </div>
       <span>You have {n} upcoming trips!</span>
