63b3ef9cb6f2c27dfd3fe93c5487f78e377c3b21
Jamie Smart <jamie.smart.10@gmail.com>
2025-07-07 11:47:46 -0400

    change attendees type to CrewMember[]

--- client/src/lib/types/trip.ts
+++ client/src/lib/types/trip.ts
@@ -1,9 +1,9 @@
-import { User } from './user'
+import { CrewMember } from './user'
 
 export interface Trip {
   id: string
   name: string
   startDate: Date
   endDate?: Date
-  attendees: User[]
+  attendees: CrewMember[]
 }
