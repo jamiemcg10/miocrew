fc0e4efa6cfb127feabc8478009638c1922838df
Jamie Smart <jamie.smart.10@gmail.com>
2025-07-07 10:34:43 -0400

    add page for missing trips

---
+++ client/src/app/(app)/trip/[tripid]/not-found.tsx
@@ -0,0 +1,14 @@
+import NoLuggageTwoToneIcon from '@mui/icons-material/NoLuggageTwoTone'
+import Button from '@mui/material/Button'
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col p-16 space-y-4" style={{ alignItems: 'center' }}>
+      <div className="text-3xl">Sorry, we can't find that trip</div>
+      <NoLuggageTwoToneIcon sx={{ fontSize: 120 }} />
+      <Button href="dashboard" sx={{ width: 'fit-content' }}>
+        Return to Dashboard
+      </Button>
+    </div>
+  )
+}
