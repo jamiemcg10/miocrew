f0ccd5cf4a08d020da1fbd92303eb1b2b3e1765e
Jamie Smart <jamie.smart.10@gmail.com>
2025-07-08 18:11:46 -0400

    add cost to add idea dialog

--- client/src/lib/components/ideas/AddIdeaDialog.tsx
+++ client/src/lib/components/ideas/AddIdeaDialog.tsx
@@ -3,6 +3,10 @@ import DialogTitle from '@mui/material/DialogTitle'
 import TextField from '@mui/material/TextField'
 import { Dispatch, SetStateAction } from 'react'
 import Dialog from '../Dialog'
+import RadioGroup from '@mui/material/RadioGroup'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import Radio from '@mui/material/Radio'
+import InputAdornment from '@mui/material/InputAdornment'
 
 interface AddIdeaDialogProps {
   open: boolean
@@ -15,8 +19,22 @@ export default function AddIdeaDialog({ open, setOpen }: AddIdeaDialogProps) {
       <DialogTitle sx={{ fontWeight: 700 }}>Add Idea</DialogTitle>
       <form className="flex flex-col m-10 mt-5">
         <TextField label="Name" required />
-        <TextField label="Link" sx={{ my: 2 }} />
-        <TextField label="Description" multiline rows={3} />
+        <TextField label="Link" sx={{ my: 2 }} size="small" />
+        <div className="inline-flex">
+          <TextField
+            label="Cost"
+            slotProps={{
+              input: { startAdornment: <InputAdornment position="start">$</InputAdornment> }
+            }}
+            sx={{ width: 100, mr: 2 }}
+            size="small"
+          />
+          <RadioGroup row>
+            <FormControlLabel label="Each" value="each" control={<Radio size="small" />} />
+            <FormControlLabel label="Total" value="total" control={<Radio size="small" />} />
+          </RadioGroup>
+        </div>
+        <TextField label="Description" multiline rows={3} sx={{ mt: 2 }} />
         <Button variant="contained" sx={{ fontWeight: 700, mt: 5 }}>
           Add Idea
         </Button>
