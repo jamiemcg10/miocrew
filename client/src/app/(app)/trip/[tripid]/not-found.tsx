e45271dad9c7bbee47f1245c988fd367ac645f1d
Jamie Smart <jamie.smart.10@gmail.com>
2025-07-09 16:18:23 -0400

    fix unescaped quots

--- client/src/app/(app)/trip/[tripid]/not-found.tsx
+++ client/src/app/(app)/trip/[tripid]/not-found.tsx
@@ -4,7 +4,7 @@ import Button from '@mui/material/Button'
 export default function NotFound() {
   return (
     <div className="flex flex-col p-16 space-y-4" style={{ alignItems: 'center' }}>
-      <div className="text-3xl">Sorry, we can't find that trip</div>
+      <div className="text-3xl">{"Sorry, we can't find that trip"}</div>
       <NoLuggageTwoToneIcon sx={{ fontSize: 120 }} />
       <Button href="dashboard" sx={{ width: 'fit-content' }}>
         Return to Dashboard
--- client/src/stories/Page.tsx
+++ client/src/stories/Page.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from 'react'
 
-import { Header } from './Header';
-import './page.css';
+import { Header } from './Header'
+import './page.css'
 
 type User = {
-  name: string;
-};
+  name: string
+}
 
 export const Page: React.FC = () => {
-  const [user, setUser] = React.useState<User>();
+  const [user, setUser] = React.useState<User>()
 
   return (
     <article>
@@ -35,8 +35,9 @@ export const Page: React.FC = () => {
         </p>
         <ul>
           <li>
-            Use a higher-level connected component. Storybook helps you compose such data from the
-            "args" of child component stories
+            {
+              'Use a higher-level connected component. Storybook helps you compose such data from the "args" of child component stories'
+            }
           </li>
           <li>
             Assemble data in the page component from your services. You can mock these services out
@@ -69,5 +70,5 @@ export const Page: React.FC = () => {
         </div>
       </section>
     </article>
-  );
-};
+  )
+}
