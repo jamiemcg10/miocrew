ce1ea695325813e3464875fc343c27a27b5ca182
Jamie Smart <jamie.smart.10@gmail.com>
2025-07-30 16:44:14 -0400

    add tripId to tripEvent

--- client/src/lib/types/tripEvent.ts
+++ client/src/lib/types/tripEvent.ts
@@ -1,5 +1,6 @@
 export interface TripEvent {
   id: string
+  tripId: string
   name: string
   description?: string
   location: string
--- client/src/lib/utils/dummyData/tripEvents.ts
+++ client/src/lib/utils/dummyData/tripEvents.ts
@@ -3,6 +3,7 @@ import { TripEvent } from '@/lib/types/tripEvent'
 export const tripEvents = [
   {
     id: 'wm1',
+    tripId: '1',
     name: 'Check into AirBnB and relax',
     description:
       'Arrive, hang out at AirBnB until dinner. Early arrivers can start scoping out what we need',
@@ -12,6 +13,7 @@ export const tripEvents = [
   },
   {
     id: 'wm2',
+    tripId: '1',
     name: 'Dinner at Gran Turismo',
     location: '132 Sonoma Crest Road',
     startTime: new Date('August 5, 2026 6:30 PM'),
@@ -20,6 +22,7 @@ export const tripEvents = [
   },
   {
     id: 'wm3',
+    tripId: '1',
     name: 'Napa Wine Tour',
     description: 'Bus tour with stops at 5 different vineyards',
     location: '11824 Main St',
@@ -29,6 +32,7 @@ export const tripEvents = [
   },
   {
     id: 'wm4',
+    tripId: '1',
     name: 'Lunch at Josh Vineyards',
     location: '414 Boston Ave.',
     startTime: new Date('August 7, 2026 12:00 PM'),
@@ -37,6 +41,7 @@ export const tripEvents = [
   },
   {
     id: 'wm5',
+    tripId: '1',
     name: 'Club Xtrove',
     location: '4155 Elm St.',
     startTime: new Date('August 7, 2026 10:00 PM'),
@@ -45,6 +50,7 @@ export const tripEvents = [
   },
   {
     id: 'wm6',
+    tripId: '1',
     name: 'Check out of AirBnB',
     location: '132 Sonoma Crest Road Ext',
     startTime: new Date('August 12, 2026 11:00 AM'),
@@ -52,6 +58,7 @@ export const tripEvents = [
   },
   {
     id: 'wm7',
+    tripId: '1',
     name: 'Winery hopping down Main Street',
     location: '416 Boston Ave.',
     description: 'Go see the best of what Napa has to offer. Eat dinner somewhere in here.',
