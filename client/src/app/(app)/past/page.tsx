35a895cec135cac8a0a17b5d95c20303469423a4
Jamie Smart <jamie.smart.10@gmail.com>
2025-12-17 11:35:50 -0500

    put past trips in local storage

--- client/src/app/(app)/past/page.tsx
+++ client/src/app/(app)/past/page.tsx
@@ -4,16 +4,14 @@ import { getTrips } from '@/db'
 import TripTable from '@/lib/components/TripTable'
 import { Trip } from '@/lib/types'
 import { UserContext } from '@/lib/utils/contexts/UserContext'
+import { LocalStorage } from '@/lib/utils/LocalStorage'
 import { useContext, useState, useEffect } from 'react'
 
 export default function PastTripsPage() {
   const user = useContext(UserContext)
 
-  const [pastTrips, setPastTrips] = useState([])
-
-  useEffect(() => {
-    if (!user) return
-    getTrips({ userId: user.id })
+  function fetchPastTrips() {
+    getTrips({ userId: user!.id })
       .then((response) => {
         const _pastTrips = response.data.trips.filter((trip: Trip) => {
           const startDate = new Date(trip.startDate)
@@ -21,8 +19,19 @@ export default function PastTripsPage() {
         })
 
         setPastTrips(_pastTrips)
+        LocalStorage.set('past-trips', _pastTrips)
       })
       .catch((e) => console.error('Error fetching past trips', e))
+  }
+
+  const storedPastTrips = LocalStorage.get('past-trips')
+
+  const [pastTrips, setPastTrips] = useState(storedPastTrips || [])
+
+  useEffect(() => {
+    if (!user) return
+
+    fetchPastTrips()
   }, [user])
 
   if (!user) return
