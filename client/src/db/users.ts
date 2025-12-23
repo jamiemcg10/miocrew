c6840e437c3dc66fcbfdc9364cc4e96e13db596a
Jamie Smart <jamie.smart.10@gmail.com>
2025-12-22 20:47:19 -0500

    add id param to getUser fn

--- client/src/db/users.ts
+++ client/src/db/users.ts
@@ -4,6 +4,8 @@ export async function getUsers() {
   return axios.get(`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/users/`, { withCredentials: true })
 }
 
-export async function getUser() {
-  return axios.get(`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/user/2`, { withCredentials: true }) // user hardcoded for now
+export async function getUser(id: string) {
+  return axios.get(`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/user/${id}`, {
+    withCredentials: true
+  })
 }
