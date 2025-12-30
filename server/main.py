dcb512cd69c27eb366f9c1c23b7ae80126bb32bc
Jamie Smart <jamie.smart.10@gmail.com>
2025-12-30 15:12:30 -0500

    use tested regex

--- server/main.py
+++ server/main.py
@@ -25,7 +25,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],
-    allow_origin_regex="https://miocrew.*\.jamiemcg10s-projects\.vercel\.app",
+    allow_origin_regex="https://miocrew-.*-jamiemcg10s-projects\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
