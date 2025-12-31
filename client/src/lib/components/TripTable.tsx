8bb8654dee6ecc40514911b2289ae91f1b01b9e9
Jamie Smart <jamie.smart.10@gmail.com>
2025-12-31 00:51:27 -0500

    use tripSort in TripTable

--- client/src/lib/components/TripTable.tsx
+++ client/src/lib/components/TripTable.tsx
@@ -7,6 +7,7 @@ import { AvatarGroup } from '@heroui/avatar'
 import CrewAvatar from './CrewAvatar'
 import TableRow from './layout/TableRow'
 import DashboardSectionHeader from '@/app/(boundary)/(app)/dashboard/DashboardSectionHeader'
+import { tripSort } from '../utils/sortFns'
 
 interface TripTableProps {
   trips: Trip[]
@@ -20,38 +21,33 @@ export default function TripTable({ trips, title }: TripTableProps) {
     <div>
       <DashboardSectionHeader title={title} />
       {trips.length ? (
-        trips
-          .sort((a, b) => (a.startDate > b.startDate ? 1 : -1)) // TODO: use trip sort here
-          .map((trip) => {
-            return (
-              <Link href={`/trip/${trip.id}`} key={trip.id}>
-                <TableRow>
-                  <div className="flex grow justify-between mr-4 flex-col sm:flex-row">
-                    <span className="px-2 inline-flex font-semibold">{trip.name}</span>
-                    <span className="text-right px-2 inline-flex font-semibold">
-                      {dateFormatter(trip.startDate)}{' '}
-                      {trip.endDate && ' - ' + dateFormatter(trip.endDate)}
-                    </span>
-                  </div>
-                  <span className="px-2 basis-1/4 sm:basis-1/5 inline-flex items-center shrink-0">
-                    <AvatarGroup isBordered max={6} classNames={avatarGroupClasses}>
-                      {Object.values(trip.attendees)
-                        .slice(0, 5)
-                        .map((user) => {
-                          return <CrewAvatar user={user} size="xs" key={user.id} />
-                        })}
-                      {Object.values(trip.attendees).length > 5 ? (
-                        <CrewAvatar
-                          name={`+${Object.values(trip.attendees).length - 5}`}
-                          size="xs"
-                        />
-                      ) : null}
-                    </AvatarGroup>
+        trips.sort(tripSort).map((trip) => {
+          return (
+            <Link href={`/trip/${trip.id}`} key={trip.id}>
+              <TableRow>
+                <div className="flex grow justify-between mr-4 flex-col sm:flex-row">
+                  <span className="px-2 inline-flex font-semibold">{trip.name}</span>
+                  <span className="text-right px-2 inline-flex font-semibold">
+                    {dateFormatter(trip.startDate)}{' '}
+                    {trip.endDate && ' - ' + dateFormatter(trip.endDate)}
                   </span>
-                </TableRow>
-              </Link>
-            )
-          })
+                </div>
+                <span className="px-2 basis-1/4 sm:basis-1/5 inline-flex items-center shrink-0">
+                  <AvatarGroup isBordered max={6} classNames={avatarGroupClasses}>
+                    {Object.values(trip.attendees)
+                      .slice(0, 5)
+                      .map((user) => {
+                        return <CrewAvatar user={user} size="xs" key={user.id} />
+                      })}
+                    {Object.values(trip.attendees).length > 5 ? (
+                      <CrewAvatar name={`+${Object.values(trip.attendees).length - 5}`} size="xs" />
+                    ) : null}
+                  </AvatarGroup>
+                </span>
+              </TableRow>
+            </Link>
+          )
+        })
       ) : (
         <div>No trips</div>
       )}
