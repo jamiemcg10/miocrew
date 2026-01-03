b04c0a704f1147b955a7be51af05bd12e488290b
Jamie Smart <jamie.smart.10@gmail.com>
2026-01-02 19:33:24 -0500

    use canonical classes in Ideas

--- client/src/lib/components/ideas/Ideas.tsx
+++ client/src/lib/components/ideas/Ideas.tsx
@@ -53,9 +53,9 @@ export default function Ideas({ setOpenAddDialog }: IdeasProps) {
         Add idea
       </Button>
       <div className="vertical-scroll relative overflow-y-auto px-8 md:px-0">
-        <div className="sticky -top-1 z-1 w-full h-3 bg-linear-to-b from-(--background) to-transparent"></div>
+        <div className="sticky -top-1 z-1 w-full h-3 bg-linear-to-b from-background to-transparent"></div>
         <div className="relative overflow-y-hidden p-4">
-          <div className="relative grid grid-cols-[repeat(1,_1fr)] md:grid-cols-[repeat(2,_1fr)] lg:grid-cols-[repeat(3,_1fr)] 2xl:grid-cols-[repeat(4,_1fr)] gap-6 md:gap-4">
+          <div className="relative grid grid-cols-[repeat(1,1fr)] md:grid-cols-[repeat(2,1fr)] lg:grid-cols-[repeat(3,1fr)] 2xl:grid-cols-[repeat(4,1fr)] gap-6 md:gap-4">
             {ideas.map((idea) => {
               return (
                 <IdeaCard
@@ -71,7 +71,7 @@ export default function Ideas({ setOpenAddDialog }: IdeasProps) {
             })}
           </div>
         </div>
-        <div className="sticky -bottom-1 w-full h-3 bg-linear-to-t from-(--background) to-transparent"></div>
+        <div className="sticky -bottom-1 w-full h-3 bg-linear-to-t from-background to-transparent"></div>
       </div>
       <ActiveIdea activeIdea={activeIdea} setActiveIdea={setActiveIdea}></ActiveIdea>
     </>
