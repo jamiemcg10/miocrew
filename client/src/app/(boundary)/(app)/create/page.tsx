e0a5a7eaad3fa7814d3a53fbd0eaf9aebd340b5b
Jamie Smart <jamie.smart.10@gmail.com>
2026-01-04 13:28:12 -0500

    adjust TripForm spacing

--- client/src/app/(boundary)/(app)/create/page.tsx
+++ client/src/app/(boundary)/(app)/create/page.tsx
@@ -4,8 +4,8 @@ import TripForm from './TripForm'
 
 export default function CreatePage() {
   return (
-    <div className="vertical-scroll flex flex-col space-y-4 px-8 py-16 sm:px-16 sm:pb-8 overflow-y-auto">
-      <div className="mb-8 font-bold text-3xl text-(--foreground) dark:text-[#90caf9]">
+    <div className="vertical-scroll flex flex-col space-y-4 px-8 pt-16 pb-8 sm:px-16 sm:pb-8 overflow-y-auto">
+      <div className="mb-8 font-bold text-3xl text-foreground dark:text-[#90caf9]">
         Create new trip
       </div>
       <TripForm />
